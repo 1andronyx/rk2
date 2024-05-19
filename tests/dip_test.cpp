03a0877c84454dbacda7ee4f615091dd92ee9de7
1andronyx <andronyxjr@gmail.com>
2024-05-19 17:18:19 +0300

    at12

--- tests/dip_test.cpp
+++ tests/dip_test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "DIP.cpp"
+#include "../DIP.cpp"
 
 class DIPTest_AntiConceptualExample_Test : public ::testing::Test {
 protected:
@@ -15,10 +15,36 @@ protected:
     void TearDown() override {}
 };
 
+class DIPTest_Third_Test : public ::testing::Test {
+protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
 TEST_F(DIPTest_AntiConceptualExample_Test, TestBody) {
-    test_anti_conceptual_example_dip();
+    DependencyInversionPrinciple01::test_anti_conceptual_example_dip();
 }
 
 TEST_F(DIPTest_ConceptualExample_Test, TestBody) {
-    test_conceptual_example_dip();
+    DependencyInversionPrinciple02::test_conceptual_example_dip();
+}
+
+TEST_F(DIPTest_Third_Test, TestBody) {
+    using namespace DependencyInversionPrinciple01;
+
+    Person parent{ "John" };
+    Person child1{ "Carina" };
+    Person child2{ "Mary" };
+
+    Relationships relationships{};
+
+    relationships.addParentAndChild(parent, child1);
+    relationships.addParentAndChild(parent, child2);
+
+    // Проверяем, что отношения добавлены корректно
+    ASSERT_EQ(relationships.m_relations.size(), 4);
+
+    // Проверяем, что дети добавлены корректно
+    ASSERT_EQ(relationships.findAllChildrenOf("John").size(), 2);
 }
